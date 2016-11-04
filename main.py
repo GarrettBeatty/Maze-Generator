fcc78081d83729bb95e0565d6a633e657005ee74
Garrett Beatty <gbt@localhost.localdomain>
2016-11-03 21:53:23 -0400

    seperated classes

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from maze import Maze
+
+root = tk.Tk()
+canvas = tk.Canvas(root, width=1000, height=800)
+canvas.pack()
+
+
+root.mainloop()
--- maze.py
+++ maze.py
@@ -1,4 +1,10 @@
-import tkinter
+class Maze:
 
-top = tkinter.Tk()
-top.mainloop()
+	def __init__(self, cols_count, rows_count):
+		self.maze = [[0 for x in range(cols_count)] for x in range(rows_count)]
+
+	def generate(self):
+		pass
+
+	def getMaze(self):
+		return self.maze
