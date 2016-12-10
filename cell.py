cce01340a1dafce4400ea7c181e35153836d6aef
Garrett Beatty <gman9732@gmail.com>
2016-12-09 22:56:44 -0500

    Added dfs

--- cell.py
+++ cell.py
@@ -5,12 +5,26 @@ class Cell:
 	def __init__(self,x,y):
 		self.x = x
 		self.y = y
+		self.top = True
+		self.bot = True
+		self.right = True
+		self.left = True
+		self.visited = False
 
 	def get_walls(self, game):
+		walls = []
+		active = [self.top, self.bot, self.left, self.right]
 		wall1 = [(self.x  * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE)), (self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE))]
-		wall2 = [(self.x  * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE)), (self.x * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
-		wall3 = [(self.x  * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE), (self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
-		wall4 = [(self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE)), (self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
+		walls.append(wall1)
 
-		return wall1,wall2,wall3,wall4
+		wall3 = [(self.x  * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE), (self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
+		walls.append(wall3)
+
+		wall2 = [(self.x  * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE)), (self.x * (game.BLOCK_SIZE), self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
+		walls.append(wall2)
+
+		wall4 = [(self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE)), (self.x  * (game.BLOCK_SIZE) + game.BLOCK_SIZE, self.y * (game.BLOCK_SIZE) + game.BLOCK_SIZE)]
+		walls.append(wall4)
+
+		return walls, active
 		
--- maze.py
+++ maze.py
@@ -1,34 +1,82 @@
 import pygame
 import random
 import math
+import numpy
 from cell import Cell
+
+class Generator:
+
+	def __init__(self):
+		pass
+
+	def dfs(self, game,x,y):
+		game.getNextFrame()
+		if game.cells[x][y].visited: return game.cells
+		game.cells[x][y].visited = True
+		
+		num_neighbors = 4
+
+		while num_neighbors != 0:
+		
+			r = random.randint(0,3)
+			
+			if  r == 0:
+				if x + 1 <= game.NUM_SQUARES - 1:
+					if not game.cells[x+1][y].visited:
+						game.cells[x+1][y].left = False
+						game.cells[x][y].right = False
+						game.cells = self.dfs(game, x + 1, y)
+			if  r == 1:
+				if x - 1 >= 0:
+					if not game.cells[x-1][y].visited:
+						game.cells[x-1][y].right = False
+						game.cells[x][y].left = False
+						game.cells = self.dfs(game, x - 1, y)
+			if  r == 2:
+				if y + 1 <= game.NUM_SQUARES - 1:
+					if not game.cells[x][y+1].visited:
+						game.cells[x][y+1].top = False
+						game.cells[x][y].bot = False
+						game.cells = self.dfs(game, x, y+1)
+			if  r == 3:
+				if y - 1 >= 0:
+					if not game.cells[x][y-1].visited:
+						game.cells[x][y-1].bot = False
+						game.cells[x][y].top = False
+						game.cells = self.dfs(game, x, y-1)
+				
+			num_neighbors = 0
+			if x + 1 <= game.NUM_SQUARES - 1:
+				if not game.cells[x+1][y].visited: num_neighbors += 1
+			if x - 1 >= 0:
+				if not game.cells[x-1][y].visited: num_neighbors += 1
+			if y + 1 <= game.NUM_SQUARES - 1:
+				if not game.cells[x][y+1].visited: num_neighbors += 1
+			if y - 1 >= 0:
+				if not game.cells[x][y-1].visited: num_neighbors += 1
+		return game.cells
 			
 class Game:
 
-	def init_cells(self):
-		for y in range(self.NUM_SQUARES):
-			for x in range(self.NUM_SQUARES):
-				c = Cell(x,y)
-				self.cells.append(c)
-
-
 	def __init__(self):
-
-		self.BLOCK_SIZE = 40
-		self.NUM_SQUARES = 20
+	
+		self.WINDOW_WIDTH = 800
+		self.WINDOW_HEIGHT = 800
+		self.NUM_SQUARES = 30
+		self.BLOCK_SIZE = self.WINDOW_WIDTH / self.NUM_SQUARES
 		self.WHITE  = (255,255,255)
 		self.BLACK = (0,0,0)
-		self.WINDOW_WIDTH = (self.BLOCK_SIZE) * self.NUM_SQUARES + 40
-		self.WINDOW_HEIGHT = (self.BLOCK_SIZE) * self.NUM_SQUARES + 40
-
-		self.cells = []
-		self.init_cells()
+		self.cells = [[Cell(i,j) for j in range(self.NUM_SQUARES)] for i in range(self.NUM_SQUARES)]
 
 	def draw_board(self):
-		for c in self.cells:
-			walls = c.get_walls(self)
-			for wall in walls:
-				pygame.draw.line(self.window, self.WHITE, wall[0], wall[1])
+		for row in self.cells:
+			for col in row:
+				walls, active = col.get_walls(self)
+				for i in range(len(walls)):
+					if active[i]:
+						pygame.draw.line(self.window, self.WHITE, walls[i][0], walls[i][1])
+					else:
+						pygame.draw.line(self.window, self.BLACK, walls[i][0], walls[i][1])
 
 
 	def getPresentFrame(self):
@@ -50,12 +98,12 @@ class Game:
 		self.window = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
 		self.getPresentFrame()
 		running = True
+		self.gen = Generator()
+		self.cells = self.gen.dfs(self, 0,0)
 		while running:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
-				else:
-					self.getNextFrame()
 
 		pygame.quit()
 
